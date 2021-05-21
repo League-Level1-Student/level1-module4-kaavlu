fd6f64b319f24d85f333e4d48c9614c65ecb811c
kaavlu <42904778+kaavlu@users.noreply.github.com>
2021-05-21 16:57:26 -0700

    worked on tweet

--- src/_09_latest_tweet/Tweet.java
+++ src/_09_latest_tweet/Tweet.java
@@ -1,5 +1,23 @@
 package _09_latest_tweet;
 
-public class Tweet {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+public class Tweet implements ActionListener{
+	JTextField input = new JTextField();
+	JButton button = new JButton();
+	
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
